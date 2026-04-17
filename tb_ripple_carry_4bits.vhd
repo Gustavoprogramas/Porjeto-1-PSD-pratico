a90476cbf3c2908db0933a6308653b3aaf151e3d
Gustavoprogramas <gustavo.vimacedo@gmail.com>
2026-04-17 12:36:20 -0300

    Refactor tb_ripple_carry_4bits for new interface
    Updated testbench to include switch inputs and LED outputs.

--- tb_ripple_carry_4bits.vhd
+++ tb_ripple_carry_4bits.vhd
@@ -2,36 +2,30 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity tb_ripple_carry_4bits is
-    end tb_ripple_carry_4bits;
+    port (
+        SW   : in  std_logic_vector(10 downto 0);
+        LEDR : out std_logic_vector(5 downto 0)
+    );
+end tb_ripple_carry_4bits;
 
-    architecture simulacao of tb_ripple_carry_4bits is
-        component ripple_carry_4bits port (
-            A, B : in std_logic_vector(3 downto 0);
+architecture bhv of tb_ripple_carry_4bits is
+    
+    component ripple_carry_4bits
+        port (
+            A, B      : in std_logic_vector(3 downto 0);
             Operation : in std_logic;
             Resultado : out std_logic_vector(3 downto 0);
-            Cout : out std_logic;
-            Overflow : out std_logic
+            Cout      : out std_logic;
+            Overflow  : out std_logic
         );
-        end component;
-        signal A_tb, B_tb : std_logic_vector(3 downto 0); := "0000";
-        signal Operation_tb : std_logic := '0';
-        signal Resultado_tb : std_logic_vector (3 downto 0);
-        signal Cout_tb, Overflow_tb : std_logic;
-        begin
-            DUT: ripple_carry_4bits port map (
-                A => A_tb,
-                B => B_tb,
-                Operation => Operation_tb,
-                Resultado => Resultado_tb,
-                Cout => Cout_tb,
-                Overflow => Overflow_tb
-            );
-            process begin
-                A_tb <= "0010"; B_tb <= "0011"; Operation_tb <= '0'; wait for 100ps; --soma sem overflow
-                A_tb <= "0111"; B_tb <= "0001"; Operation_tb <= '0'; wait for 100ps; --soma com overflow
-                A_tb <= "0101"; B_tb <= "0011"; Operation_tb <= '1'; wait for 100ps; --subtração sem overflow
-                A_tb <= "0011"; B_tb <= "0101"; Operation_tb <= '1'; wait for 100ps; --subtração gerando negativo
-                wait;
-            end process;
-    end simulacao;
-
+    end component;
+begin
+    U1: ripple_carry_4bits port map (
+        A         => SW(10 downto 7),
+        B         => SW(6 downto 3),
+        Operation => SW(0),
+        Resultado => LEDR(3 downto 0),
+        Cout      => LEDR(4),
+        Overflow  => LEDR(5)
+    );
+end bhv;
