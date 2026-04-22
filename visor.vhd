a9e3041eed20a6f6e0a33a5d595ffd1c93b5336f
Gustavoprogramas <gustavo.vimacedo@gmail.com>
2026-04-22 16:38:31 -0300

    Update visor.vhd

--- visor.vhd
+++ visor.vhd
@@ -3,8 +3,8 @@ use ieee.std_logic_1164.all;
 
 entity visor is
 port (
-			bin : in std_logic_vector(3 downto 0); --codigo binario
-			seg : out std_logic_vector(6 downto 0) --segmentos do visor de a até g
+			bin : in std_logic_vector(3 downto 0); --numeros em binario
+			seg : out std_logic_vector(0 to 6) --segmentos do visor de a até g
 			);
 end visor;
 
@@ -13,17 +13,17 @@ begin
 		process(bin)
 		begin
 				case bin is
-				when "0000" => seg <= "1000000";
-				when "0001" => seg <= "1111001";
-				when "0010" => seg <= "0100100";
-				when "0011" => seg <= "0110000";
-				when "0100" => seg <= "0110001";
-				when "0101" => seg <= "0010010";
-				when "0110" => seg <= "0000010";
-				when "0111" => seg <= "1111000";
+				when "0000" => seg <= "0000001";
+				when "0001" => seg <= "1001111";
+				when "0010" => seg <= "0010010";
+				when "0011" => seg <= "0000110";
+				when "0100" => seg <= "1001100";
+				when "0101" => seg <= "0100100";
+				when "0110" => seg <= "0100000";
+				when "0111" => seg <= "0001111";
 				when "1000" => seg <= "0000000";
-				when "1001" => seg <= "0010000";
-				when "1010" => seg <= "0001000";
+				when "1001" => seg <= "0000100"; --arrumado ate aqui
+				when "1010" => seg <= "0001000"; --aqui usa o hex 7, arrumar!
 				when "1011" => seg <= "0000011";
 				when "1100" => seg <= "1000110";
 				when "1101" => seg <= "0100001";
