1307a083c582b0b1036e80bd47f3d02773962cd4
Gustavoprogramas <gustavo.vimacedo@gmail.com>
2026-05-20 08:15:44 -0300

    Update ULA.vhd

--- ULA.vhd
+++ ULA.vhd
@@ -61,18 +61,18 @@ begin
     );
 
 
-    with AluOp select
+    with AluOp select --MUX baseado no valor dos SW0 - SW2
         result_mux <= (A and B) when "001", -- Porta AND
                       (A or B)  when "010", -- Porta OR
                       (not B)   when "011", -- Porta NOT
                       res_soma  when "100", -- Soma
                       res_soma  when "101", -- Subtração
                       res_mult  when "110", -- Multiplicação
-                      "0000"    when others; -- NOP (000), COMP (111) ou indefinido
+                      "0000"    when others; -- NOP 
 
     Result <= result_mux;
 
-
+-- flags para zerar o resultado do visor
     Zero     <= '1'       when (result_mux = "0000" and AluOp /= "000") else '0';
     CarryOut <= cout_int  when (AluOp = "100" or AluOp = "101") else '0';
     Overflow <= ovf_int   when (AluOp = "100" or AluOp = "101") else '0';
